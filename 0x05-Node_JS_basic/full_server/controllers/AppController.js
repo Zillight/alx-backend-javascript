89bbb6035a8c001da55e81f77a66cc607cac95c0
Banji Zillight Olawole <107891531+Zillight@users.noreply.github.com>
2024-08-19 17:28:23 +0100

    user cntrl

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,7 @@
-#AppController
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
