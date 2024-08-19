0e0c4e9d63a760b7983ecb260c90fc30d7796ee5
Banji Zillight Olawole <107891531+Zillight@users.noreply.github.com>
2024-08-19 17:31:14 +0100

    index

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,2 +1,13 @@
-#Index
-  
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+const router = express.Router();
+
+router.get('/', AppController.getHomepage);
+
+router.get('/students', StudentsController.getAllStudents);
+
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+module.exports = router;
